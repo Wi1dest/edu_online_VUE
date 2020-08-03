f140378f0bd67f6eed550fd0ee05c46ac985ea0e
wi1dest <loshungan@hotmail.com>
2020-08-04 01:12:20 +0800

    模拟登录,讲师列表展示

---
+++ src/api/edu/teacher.js
@@ -0,0 +1,15 @@
+import request from '@/utils/request'
+
+export default {
+    // current:当前页 limit:每页记录数 teacherVO:查询条件
+    getTeacherListPage(current,limit,teacherVO){
+        return request({
+            url: `/eduservice/teacher/pageTeacherCondition/${current}/${limit}`,
+            method: 'post',
+            // teacherVO查询条件
+            // data:把对象转成JSON传给后端
+            data: teacherVO
+          })
+    }
+}
+

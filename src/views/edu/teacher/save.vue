1b7bc98e2a96b68b579019d660997ae584081002
wi1dest <loshungan@hotmail.com>
2020-08-04 20:21:05 +0800

    头像上传

--- src/views/edu/teacher/save.vue
+++ src/views/edu/teacher/save.vue
@@ -24,7 +24,30 @@
         <el-input v-model="teacher.intro" :rows="10" type="textarea" />
       </el-form-item>
 
-      <!-- 讲师头像：TODO -->
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+        <!-- 头衔缩略图 -->
+        <pan-thumb :image="teacher.avatar" />
+        <!-- 文件上传按钮 -->
+        <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像</el-button>
+
+        <!--
+        v-show：是否显示上传组件
+        :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+        :url：后台上传的url地址
+        @close：关闭上传组件
+        @crop-upload-success：上传成功后的回调-->
+        <image-cropper
+          v-show="imagecropperShow"
+          :width="300"
+          :height="300"
+          :key="imagecropperKey"
+          :url="BASE_API+'/ossservice/file'"
+          field="file"
+          @close="close"
+          @crop-upload-success="cropSuccess"
+        />
+      </el-form-item>
 
       <el-form-item>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate()">保存</el-button>
@@ -35,8 +58,14 @@
 
 <script>
 import teacher from "@/api/edu/teacher.js";
+import ImageCropper from "@/components/ImageCropper";
+import PanThumb from "@/components/PanThumb";
 
 export default {
+  components: {
+    ImageCropper,
+    PanThumb,
+  },
   data() {
     return {
       teacher: {
@@ -47,6 +76,9 @@ export default {
         intro: "",
         avatar: "",
       },
+      imagecropperShow: false,
+      imagecropperKey: 0,
+      BASE_API: process.env.BASE_API,
       saveBtnDisabled: false,
     };
   },
@@ -117,6 +149,16 @@ export default {
             message: "保存失败",
           });
         });
+    },
+    // 关闭上传弹窗
+    close() {
+        this.imagecropperShow = false
+    },
+    //上传成功方法
+    cropSuccess(data) {
+        this.imagecropperShow = false
+        //上传之后接口返回的地址
+        this.teacher.avatar = data.url
     },
   },
 };
